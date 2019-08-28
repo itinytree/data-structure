940a6219c130444ae6696257752386fb5ab56dd4
小树 <tinytree@yeah.net>
2019-08-28 14:20:22 +0800

    完成数组的实现与测试

--- Arrays/src/main/java/me/qianlv/Array.java
+++ Arrays/src/main/java/me/qianlv/Array.java
@@ -1,5 +1,7 @@
 package me.qianlv;
 
+import java.util.Objects;
+
 /**
  * 数组
  *
@@ -53,9 +55,177 @@ public class Array<E> {
         return size == 0;
     }
 
+    /**
+     * 在idnex索引的位置插入一个新元素e
+     *
+     * @param index
+     * @param e
+     */
     public void add(int index, E e) {
         if (index < 0 || index > size) {
-
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size");
         }
+
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+
+        data[index] = e;
+        size++;
+    }
+
+    /**
+     * 在最后插入一个元素
+     *
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    /**
+     * 在所有元素的前面插入一个元素
+     *
+     * @param e
+     */
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    /**
+     * 获取index索引位置的元素
+     *
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get Failed. Index is illegal.");
+        }
+        return data[index];
+    }
+
+    /**
+     * 修改index位置的元素
+     *
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    /**
+     * 判断数组中是否包含元素e
+     *
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(data[i], e)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 查找数组中元素e所在的索引,如果不存在元素e,则返回-1
+     *
+     * @param e
+     * @return
+     */
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(data[i], e)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 从数组中删除index位置的元素,返回删除的元素
+     *
+     * @param index
+     * @return
+     */
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        }
+
+        E ret = data[index];
+        for (int i = index; i < size; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        data[size] = null;
+
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    /**
+     * 删除数组中第一个元素
+     *
+     * @return
+     */
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    /**
+     * 从数组中删除最后一个元素,返回删除的元素
+     *
+     * @return
+     */
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    /**
+     * 从数组中删除元素e
+     *
+     * @param e
+     */
+    public void removeElement(E e) {
+        int index = find(e);
+        if (-1 != index) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        sb.append("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(data[i]);
+            if (i != size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        System.arraycopy(data, 0, newData, 0, size);
+        data = newData;
     }
 }
