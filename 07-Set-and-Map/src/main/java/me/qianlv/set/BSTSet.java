2c55cc39778281c75fd0ed1759e32028291a3325
xiaoshu <tinytree@yeah.net>
2019-09-25 09:06:34 +0800

    添加 set

---
+++ 07-Set-and-Map/src/main/java/me/qianlv/set/BSTSet.java
@@ -0,0 +1,39 @@
+package me.qianlv.set;
+
+import me.qianlv.BST;
+
+/**
+ * @author xiaoshu
+ */
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+    private BST<E> bst;
+
+    public BSTSet() {
+        this.bst = new BST<>();
+    }
+
+    @Override
+    public void add(E e) {
+        bst.add(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return bst.contains(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        bst.remove(e);
+    }
+
+    @Override
+    public int getSize() {
+        return bst.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return bst.isEmpty();
+    }
+}
---
+++ 07-Set-and-Map/src/main/java/me/qianlv/set/Set.java
@@ -0,0 +1,13 @@
+package me.qianlv.set;
+
+public interface Set<E> {
+    void add(E e);
+
+    boolean contains(E e);
+
+    void remove(E e);
+
+    int getSize();
+
+    boolean isEmpty();
+}
